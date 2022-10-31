1e03a39a9a29e67466e317b8614ae0b15a5ca591
Anit <anittankandasamy@gmail.com>
2022-10-31 16:53:56 -0400

    removing repetition from components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import MatchList from './components/MatchList';
 import PlayerList from './components/PlayerList';
+import matchData from './data/matchData';
+import playerData from './data/playerData';
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <h1>
         Tourney Matches <span>Where coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList />
-      <MatchList />
+      <PlayerList matchData={matchData} playerData={playerData}/>
+      <MatchList matchData={matchData}/>
     </div>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,12 +1,10 @@
 import Player from "./Player";
-import matchData from "../data/matchData";
-import playerData from "../data/playerData";
 import {preparePlayerData, addWinsToPlayers} from '../helpers/playerHelpers';
 
 function PlayerList(props){
 
-	const playerDataArray = preparePlayerData(playerData);
-	const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+	const playerDataArray = preparePlayerData(props.playerData);
+	const parsedPlayerData = addWinsToPlayers(playerDataArray, props.matchData);
 	const onePlayer = parsedPlayerData[0];
 	console.log(playerDataArray)
 
